b6ec929c39a9af306f4bc9bb75de3e8e36d8aeba
Joris Hänseler <joris.haenseler@hispeed.ch>
2022-09-07 16:27:41 +0200

    changed name from directory traversal to path traversal

--- app/pathtraversal/enterfile.php
+++ app/pathtraversal/enterfile.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>directorytraversalwrong</title>
+    <title>pathtraversalwrong</title>
 </head>
 <body>
     <p>Enter a file wich you want to read from the file system <b>but only in the examplefiles directory</b></p>
